25041be7ce962a94b4c1e61760cf72c9bd996aba
Pier Fumagalli <ianosh@gmail.com>
2014-10-25 21:51:29 +0900

    Karma is a bitch :-)

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,33 @@
 'use strict';
 
+
 module.exports = function(config) {
 
   config.set({
     /* Base tests definition */
     basePath: '.',
-    frameworks: ['mocha', 'requirejs', 'chai'],
+    frameworks: ['requirejs', 'mocha', 'chai'],
     port: 9876,
     autoWatch: true,
     singleRun: false,
+
+    plugins: [
+      "karma-chai",
+      "karma-mocha",
+      "karma-requirejs",
+      "karma-chrome-launcher",
+      "karma-firefox-launcher",
+      "karma-phantomjs-launcher",
+      "karma-safari-launcher",
+      require('./karma.reporter')
+    ],
 
     /* These need to be in order */
     files: [
 
       /* Our libraries (in order) */
       'lib/angular-1.3.0.js',
-      'lib/angular-mocks-1.3.0.js',
+      'lib/angular-mocks-simple.js',
 
       /* Source files (angular modules) */
       { pattern: 'src/modules/*.js', included: false },
@@ -28,14 +40,10 @@ module.exports = function(config) {
 
     /* Pretty */
     logLevel: config.LOG_INFO,
-    reporters: ['mocha', 'clear-screen'],
+    reporters: ['detailed'],
     colors: true,
 
-    htmlReporter: {
-      outputDir: 'reports'
-    },
-
     /* Our browsers */
-    browsers: ['PhantomJS'], //'Chrome', 'Firefox', 'Safari'],
+    browsers: ['PhantomJS', 'Chrome'], // 'Firefox', 'Safari'],
   });
 };
