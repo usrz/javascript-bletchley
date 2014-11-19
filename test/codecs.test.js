a3d6e38699215b68c12126fcdd0ce4fdf9dffc90
Pier Fumagalli <ianosh@gmail.com>
2014-11-20 00:34:35 +0900

    Codecs tests

---
+++ test/codecs.test.js
@@ -0,0 +1,76 @@
+esquire(['bletchley/codecs'], function(codecs) {
+
+  describe("Codecs", function() {
+
+    /* String for 'Tokyo' in Japanese */
+    var tokyo = "\u6771\u4EAC";
+    /* Uint8Array of 'Tokyo' in UTF8 */
+    var tokyoArray = [230, 157, 177, 228, 186, 172];
+    /* Uint8Array of 'Tokyo' in UTF8 */
+    var tokyoUint8Array = new Uint8Array(tokyoArray);
+    /* HEX string of 'Tokyo' in UTF8 (we encode in lower case) */
+    var tokyoHex = 'e69db1e4baac';
+    /* Base 64 string of 'Tokyo' in UTF8 */
+    var tokyoB64 = '5p2x5Lqs';
+
+    /* ====================================================================== */
+    /* BASIC TESTS                                                            */
+    /* ====================================================================== */
+
+    it("should exist", function() {
+      expect(codecs).to.exist;
+      expect(codecs.algorithms).to.contain("BASE-64");
+      expect(codecs.algorithms).to.contain("HEX");
+      expect(codecs.algorithms).to.contain("UTF-8");
+    });
+
+    /* ====================================================================== */
+    /* UTF-8 TESTS                                                            */
+    /* ====================================================================== */
+
+    it("should encode in UTF8", function() {
+      var encoded = codecs.encode('UTF-8', tokyoArray);
+      expect(encoded).to.be.a('string');
+      expect(encoded).to.equal(tokyo);
+    });
+
+    it("should decode from UTF8", function() {
+      var decoded = codecs.decode('UTF-8', tokyo);
+      expect(decoded).to.be.instanceof(Uint8Array);
+      expect(decoded).to.deep.equal(tokyoUint8Array);
+    });
+
+    /* ====================================================================== */
+    /* HEX TESTS                                                              */
+    /* ====================================================================== */
+
+    it("should encode in HEX", function() {
+      var encoded = codecs.encode('HEX', tokyoArray);
+      expect(encoded).to.be.a('string');
+      expect(encoded).to.equal(tokyoHex);
+    });
+
+    it("should decode from HEX", function() {
+      var decoded = codecs.decode('HEX', tokyoHex);
+      expect(decoded).to.be.instanceof(Uint8Array);
+      expect(decoded).to.deep.equal(tokyoUint8Array);
+    });
+
+    /* ====================================================================== */
+    /* BASE_64 TESTS                                                          */
+    /* ====================================================================== */
+
+    it("should encode in BASE64", function() {
+      var encoded = codecs.encode('BASE-64', tokyoArray);
+      expect(encoded).to.be.a('string');
+      expect(encoded).to.equal(tokyoB64);
+    });
+
+    it("should decode from BASE64", function() {
+      var decoded = codecs.decode('BASE-64', tokyoB64);
+      expect(decoded).to.be.instanceof(Uint8Array);
+      expect(decoded).to.deep.equal(tokyoUint8Array);
+    });
+  });
+});
+
