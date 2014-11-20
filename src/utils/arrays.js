2c5f7f624e8879c51146b9e9f383e9657b082198
Pier Fumagalli <ianosh@gmail.com>
2014-11-20 10:54:16 +0900

    Create (and fill) Uint8Array.

--- src/utils/arrays.js
+++ src/utils/arrays.js
@@ -1,5 +1,10 @@
+'use strict';
+
 Esquire.define('bletchley/utils/arrays', [], function() {
 
+  /**
+   * Convert an Array, ArrayBuffer or ArrayBufferView into a valid Uint8Array.
+   */
   function toUint8Array(array) {
     if (! array) throw new Error("No array to convert to Uint8Array");
 
@@ -13,6 +18,11 @@ Esquire.define('bletchley/utils/arrays', [], function() {
     throw new Error("Unable to convert " + typeof(array) + " to Uint8Array");
   }
 
+  /**
+   * Silly function to convert a sequence of bytes wrapped in a "string"
+   * into an Uint8Array, so that BASE64 and UTF8 decoding can be done
+   * using native helpers.
+   */
   function fromUint8String(string) {
     if (typeof(string) === 'string') {
       var array = new ArrayBuffer(string.length);
@@ -25,6 +35,11 @@ Esquire.define('bletchley/utils/arrays', [], function() {
     throw new Error("Unable to convert " + typeof(string) + " to Uint8String");
   }
 
+  /**
+   * Silly function to convert an Uint8Array as a sequence of bytes, but
+   * wrapped in a "string", so that BASE64 and UTF8 encoding can be done
+   * using native helpers.
+   */
   function toUint8String(array) {
     array = toUint8Array(array);
     var raw = new Array(array.length);
@@ -34,10 +49,25 @@ Esquire.define('bletchley/utils/arrays', [], function() {
     return raw.join('');
   }
 
+  /**
+   * Create a new Uint8Array of the specified size, optionally filling it
+   * with the specified byte (repeated for the length of the array)
+   */
+  function createUint8Array(size, byte) {
+    var array = new Uint8Array(size);
+    if (byte != undefined) {
+      for (var i = 0; i < array.length; i ++) {
+        array[i] = byte;
+      }
+    }
+    return array;
+  }
+
   return Object.freeze({
     toUint8Array: toUint8Array,
     toUint8String: toUint8String,
-    fromUint8String: fromUint8String
+    fromUint8String: fromUint8String,
+    createUint8Array: createUint8Array
   });
 
 });
