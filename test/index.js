74a8c4d274da5980e9dd35a09e5ea9f070ffecb3
Pier Fumagalli <ianosh@gmail.com>
2014-11-29 16:43:45 +0900

    Test on worker (codecs).

--- test/index.js
+++ test/index.js
@@ -5,12 +5,13 @@ esquire(['bletchley/codecs',
          'bletchley/hmacs',
 
          'bletchley/crypto/async',
+         'bletchley/crypto/worker',
 
          'test/codecs',
          'test/hashes',
          'test/hmacs'],
   function(codecs, hashes, hmacs,
-           asyncCrypto,
+           asyncCrypto, workerCrypto,
            testCodecs, testHashes, testHMACs) {
 
     describe("Helpers implementation", function() {
@@ -21,6 +22,12 @@ esquire(['bletchley/codecs',
 
     describe("Local asynchronous crypto implementation", function() {
       testCodecs(asyncCrypto, true);
+      // testHashes(crypto);
+      // testHMACs(crypto);
+    });
+
+    describe("Worker crypto implementation", function() {
+      testCodecs(workerCrypto, true);
       // testHashes(crypto);
       // testHMACs(crypto);
     });
