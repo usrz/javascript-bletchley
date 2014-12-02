c952f6d6c2f430a68409d8a2b607008372b3022b
Pier Fumagalli <ianosh@gmail.com>
2014-12-02 20:41:43 +0900

    Accept output buffers

--- src/hmacs/HMAC.js
+++ src/hmacs/HMAC.js
@@ -9,14 +9,19 @@ Esquire.define('bletchley/hmacs/HMAC', [ 'bletchley/utils/helpers',
       this.blockSize = hash.blockSize;
       this.digestSize = hash.digestSize;
 
-      /* Inner hash can be reused for each digest size */
+      /* Inner hash and key hash can be reused */
       var innerHash = new Uint8Array(hash.digestSize);
+      var keyHash = new Uint8Array(hash.digestSize);
 
       /* HMAC calculation */
-      this.hmac = function(salt, secret) {
+      this.hmac = function(salt, secret, output) {
         salt = arrays.toUint8Array(salt);
         secret = arrays.toUint8Array(secret);
 
+        /* Create the output, if we don't have it */
+        if (!output) output = new Uint8Array(hash.digestSize);
+
+        /* Let's try to get a key for the salt (expand or hash) */
         var key;
         if (salt.length == outerPadding.length) {
           /* Salt length is good */
@@ -28,14 +33,16 @@ Esquire.define('bletchley/hmacs/HMAC', [ 'bletchley/utils/helpers',
           key = expanded;
         } else if (salt.length > outerPadding.length) {
           /* Need to hash a long salt with the algorithm */
-          key = new Uint8Array(hash.hash(salt));
+          key = hash.hash(salt, keyHash);
         }
 
+        /* Calculate the inner key padding, and get the inner hash */
         var innerKeyPadding = arrays.xorUint8Arrays(innerPadding, key);
         hash.hash(arrays.concatUint8Arrays(innerKeyPadding, secret), innerHash);
 
+        /* Calculare the outer key padding, slap result in the output and done */
         var outerKeyPadding = arrays.xorUint8Arrays(outerPadding, key);
-        return hash.hash(arrays.concatUint8Arrays(outerKeyPadding, innerHash));
+        return hash.hash(arrays.concatUint8Arrays(outerKeyPadding, innerHash), output);
       }
 
       helpers.Helper.call(this, hash.algorithm);
