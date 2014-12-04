a6a18703cf899927d462e8ffe56a1fa40a80b255
Pier Fumagalli <ianosh@gmail.com>
2014-12-04 17:05:16 +0900

    Renaming files.

--- src/hashes/Hash.js
+++ src/hashes/Hash.js
@@ -1,36 +1,22 @@
 'use strict';
 
-Esquire.define('bletchley/hashes/Hash', ['bletchley/utils/helpers',
-                                         'bletchley/utils/extend',
-                                         'bletchley/utils/arrays'],
+Esquire.define('bletchley/hashes/Hash2', ['bletchley/utils/helpers',
+                                          'bletchley/utils/extend',
+                                          'bletchley/utils/arrays' ],
   function(helpers, extend, arrays) {
 
-    var Hash = extend(function(name, blockSize, digestSize, hash) {
-      this.blockSize = blockSize;
-      this.digestSize = digestSize;
+    return extend(function(algorithm, blockSize, digestSize) {
 
-      this.hash = function(array, output, h) {
-        return hash.call(this, arrays.toUint8Array(array), output, h);
-      }
+      Object.defineProperties(this, {
+        "blockSize":  { enumerable: true, configurable: false, value: blockSize },
+        "digestSize": { enumerable: true, configurable: false, value: digestSize },
+        "hash":       { enumerable: true, configurable: false, value: function(message, output) {
+          return this.reset().update(message).finish(output);
+        }},
+      });
 
-      helpers.Helper.call(this, name);
+      helpers.Helper.call(this, algorithm);
     }, helpers.Helper, "Hash");
-
-    Object.defineProperty(Hash.prototype, "$$result", {
-      configurable: false,
-      enumerable: false,
-      value: function(output, fn) {
-        var result, view;
-        if (output) {
-          fn.call(this, new DataView(output.buffer, output.byteOffset, output.byteLength));
-          return output;
-        } else {
-          var hash = new ArrayBuffer(this.digestSize);
-          fn.call(this, new DataView(hash));
-          return new Uint8Array(hash);
-        }
-      }
-    });
 
     return Hash;
 
