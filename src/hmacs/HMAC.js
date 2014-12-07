58c0d4d3565cadc4dda05630e65e5daf3ee0950d
Pier Fumagalli <ianosh@gmail.com>
2014-12-07 20:30:59 +0900

    Bind "hmac" function.

--- src/hmacs/HMAC.js
+++ src/hmacs/HMAC.js
@@ -19,43 +19,45 @@ Esquire.define('bletchley/hmacs/HMAC', [ 'bletchley/utils/Helper',
 
       Object.defineProperties(this, {
         "blockSize":  { enumerable: true, configurable: false, value: hash.blockSize },
-        "digestSize": { enumerable: true, configurable: false, value: hash.digestSize },
-        "hmac":       { enumerable: true, configurable: false, value: function(salt, secret, output) {
-          salt = arrays.toUint8Array(salt);
-          secret = arrays.toUint8Array(secret);
-
-          /* Create the output, if we don't have it */
-          if (!output) output = new Uint8Array(hash.digestSize);
-
-          /* Let's try to get a key for the salt (expand or hash) */
-          if (salt.length == outerPadding.length) {
-
-            /* Salt length matches, just overwrite */
-            expandedSalt.set(salt, 0);
-
-          } else if (salt.length < outerPadding.length) {
-
-            /* Salt is short, expand with zeroes */
-            expandedSalt.set(salt, 0);
-            expandedSalt.set(zeroesFiller.subarray(salt.length), salt.length);
-
-          } else if (salt.length > outerPadding.length) {
-
-            /* Salt is long, hash and fill the rest with zeroes */
-            hash.hash(salt, expandedSalt);
-            expandedSalt.set(zeroesFiller.subarray(hash.digestSize), hash.digestSize);
-
-          }
-
-          /* Calculate the inner key padding, and get the inner hash */
-          arrays.xorUint8Arrays(innerPadding, expandedSalt, innerKeyPadding);
-          hash.reset().update(innerKeyPadding).update(secret).finish(innerHash);
-
-          /* Calculare the outer key padding, slap result in the output and done */
-          arrays.xorUint8Arrays(outerPadding, expandedSalt, outerKeyPadding);
-          return hash.reset().update(outerKeyPadding).update(innerHash).finish(output);
-        }}
+        "digestSize": { enumerable: true, configurable: false, value: hash.digestSize }
       });
+
+      /* HMAC function to bind */
+      this.hmac = function(salt, secret, output) {
+        salt = arrays.toUint8Array(salt);
+        secret = arrays.toUint8Array(secret);
+
+        /* Create the output, if we don't have it */
+        if (!output) output = new Uint8Array(hash.digestSize);
+
+        /* Let's try to get a key for the salt (expand or hash) */
+        if (salt.length == outerPadding.length) {
+
+          /* Salt length matches, just overwrite */
+          expandedSalt.set(salt, 0);
+
+        } else if (salt.length < outerPadding.length) {
+
+          /* Salt is short, expand with zeroes */
+          expandedSalt.set(salt, 0);
+          expandedSalt.set(zeroesFiller.subarray(salt.length), salt.length);
+
+        } else if (salt.length > outerPadding.length) {
+
+          /* Salt is long, hash and fill the rest with zeroes */
+          hash.hash(salt, expandedSalt);
+          expandedSalt.set(zeroesFiller.subarray(hash.digestSize), hash.digestSize);
+
+        }
+
+        /* Calculate the inner key padding, and get the inner hash */
+        arrays.xorUint8Arrays(innerPadding, expandedSalt, innerKeyPadding);
+        hash.reset().update(innerKeyPadding).update(secret).finish(innerHash);
+
+        /* Calculare the outer key padding, slap result in the output and done */
+        arrays.xorUint8Arrays(outerPadding, expandedSalt, outerKeyPadding);
+        return hash.reset().update(outerKeyPadding).update(innerHash).finish(output);
+      };
 
       Helper.call(this, hash.algorithm);
     };
