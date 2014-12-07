9a36f284c894ad2effa9309ca7b3be455a019afd
Pier Fumagalli <ianosh@gmail.com>
2014-12-07 20:30:23 +0900

    Bound function.

--- src/kdf/KDF.js
+++ src/kdf/KDF.js
@@ -5,13 +5,9 @@ Esquire.define('bletchley/kdfs/KDF', ['bletchley/utils/Helper',
   function(Helper, arrays) {
 
     function KDF(name, kdf) {
-      Object.defineProperty(this, "kdf", {
-        configurable: false,
-        enumerable: true,
-        value: function(password, salt, options) {
-          return kdf(arrays.toUint8Array(password), arrays.toUint8Array(salt), options);
-        }
-      });
+      this.kdf = function(password, salt, options) {
+        return kdf(arrays.toUint8Array(password), arrays.toUint8Array(salt), options);
+      }
 
       Helper.call(this, name);
     };
