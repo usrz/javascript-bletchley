9caa24c97de5786dba2eb19b820679f2801b8264
Pier Fumagalli <ianosh@gmail.com>
2014-12-09 19:52:49 +0900

    Revert "Split out Chunker from Hash."
    This reverts commit c66bc720ce9c8fda6e83278a7a35fbc4527b6cf1.

--- src/hashes/BaseSHA.js
+++ src/hashes/BaseSHA.js
@@ -2,47 +2,39 @@
 
 Esquire.define('bletchley/hashes/BaseSHA', ['bletchley/hashes/Hash'], function(Hash) {
 
-  function BaseSHA(algorithm, blockSize, digestSize, lenBytes) {
+  function BaseSHA(algorithm, buffSize, digestSize, lenBytes) {
 
     /* Final buff for updates */
-    var fbuff = new Uint8Array(blockSize);
+    var fbuff = new Uint8Array(buffSize);
     var fview = new DataView(fbuff.buffer);
-    var max = blockSize - lenBytes;
-    var off = blockSize - 4;
+    var max = buffSize - lenBytes;
+    var off = buffSize - 4;
 
     Object.defineProperty(this, "$flush", {
       configurable: true,
       enumerable: false,
       value: function(buff, pos, len, view) {
 
+        /* Append the fbuff 0x80 to the message */
+        buff[pos ++] = 0x80;
+
         /* Set the length in our fbuff chunk */
         fview.setUint32(off, len * 8, false);
 
-        /* Block limit? */
-        if (pos == blockSize) {
-          this.$compute(view);
-          fbuff[0] = 0x80;
-          this.$compute(fview);
-          return;
-        }
-
-        /* Append 0x80 to the message */
-        buff[pos ++] = 0x80;
-        fbuff[0] = 0;
-
+        /* Compute the last chunk, if needed */
         if (pos > max) {
           /* No space for the message length */
+          buff.set(fbuff.subarray(0, buffSize - pos), pos);
           this.$compute(view);
           this.$compute(fview);
         } else {
-          /* Jut add the length to the block */
           buff.set(fbuff.subarray(pos), pos);
           this.$compute(view);
         }
       }
     });
 
-    Hash.call(this, algorithm, blockSize, digestSize);
+    Hash.call(this, algorithm, buffSize, digestSize);
   }
 
   BaseSHA.prototype = Object.create(Hash.prototype);
--- src/hashes/Hash.js
+++ src/hashes/Hash.js
@@ -1,20 +1,79 @@
 'use strict';
 
-Esquire.define('bletchley/hashes/Hash', [ 'bletchley/utils/Helper', 'bletchley/utils/Chunker', 'bletchley/utils/arrays' ], function(Helper, Chunker, arrays) {
+Esquire.define('bletchley/hashes/Hash', [ 'bletchley/utils/Helper', 'bletchley/utils/arrays' ], function(Helper, arrays) {
 
   function Hash(algorithm, blockSize, digestSize) {
 
-    /* Bake in our Chunker */
-    var $this = this;
-    var chunker = new Chunker(blockSize, function(block, blockSize, totalLength, last, dataView) {
-      if (last) $this.$flush(block, blockSize, totalLength, dataView);
-      else $this.$compute(dataView);
-    });
+    var buff = new Uint8Array(blockSize);
+    var view = new DataView(buff.buffer);
+    var pos = 0;
+    var len = 0;
 
     Object.defineProperties(this, {
-      "blockSize":  { enumerable: true,  configurable: false, value: blockSize  },
-      "digestSize": { enumerable: true,  configurable: false, value: digestSize },
-      "$chunker":   { enumerable: false, configurable: false, value: chunker }
+      "blockSize":  { enumerable: true, configurable: false, value: blockSize },
+      "digestSize": { enumerable: true, configurable: false, value: digestSize },
+
+      "update": { enumerable: true, configurable: true, value: function(message) {
+
+        /* Normalize and process the message*/
+        message = arrays.toUint8Array(message);
+        var mpos = 0;
+
+        /* Copy in chunks to our block */
+        while (mpos < message.length) {
+          /* Bytes available in the block */
+          var blen = blockSize - pos;
+          /* Bytes available in the message */
+          var mlen = message.length - mpos;
+          /* Copy up to 64 bytes (nothing more) */
+          var clen = blen < mlen ? blen : mlen;
+          /* End message offset after copy */
+          var mend = mpos + clen;
+
+          /* Copy the (part of) message in our block */
+          buff.set(message.subarray(mpos, mend), pos);
+
+          /* Update length and positions */
+          len += clen;
+          pos += clen;
+          mpos = mend;
+
+          /* If we have a full block, compute it */
+          if (pos >= blockSize) {
+            this.$compute(view);
+            pos = 0;
+          }
+        }
+
+        /* Return ourselves */
+        return this;
+
+      }},
+
+      "reset": { enumerable: true, configurable: true, value: function(message) {
+        this.$reset();
+        len = 0;
+        pos = 0;
+        return this;
+      }},
+
+      "digest": { enumerable: true, configurable: true, value: function(output) {
+
+        /* No output? Create a new buffer */
+        if (! output) {
+          output = new Uint8Array(this.digestSize);
+        } else if (!(output instanceof Uint8Array)) {
+          throw new Error("Output must be a Uint8Array");
+        } else if (output.length < this.digestSize) {
+          throw new Error("Required at least " + this.digestSize + " for output");
+        }
+
+        /* Flush, get the current hash, reset, and return */
+        this.$flush(buff, pos, len, view);
+        this.$hash(new DataView(output.buffer, output.byteOffset, output.byteLength));
+        return output;
+
+      }}
     });
 
     Helper.call(this, algorithm);
@@ -26,34 +85,6 @@ Esquire.define('bletchley/hashes/Hash', [ 'bletchley/utils/Helper', 'bletchley/u
   Hash.prototype.hash = function(message, output) {
     return this.reset().update(message).digest(output);
   }
-
-  Hash.prototype.update = function(message) {
-    this.$chunker.push(message);
-    return this;
-  }
-
-  Hash.prototype.reset = function(message) {
-    this.$reset();
-    return this;
-  }
-
-  Hash.prototype.digest = function(output) {
-
-    /* No output? Create a new buffer */
-    if (! output) {
-      output = new Uint8Array(this.digestSize);
-    } else if (!(output instanceof Uint8Array)) {
-      throw new Error("Output must be a Uint8Array");
-    } else if (output.length < this.digestSize) {
-      throw new Error("Required at least " + this.digestSize + " for output");
-    }
-
-    /* Flush, get the current hash, reset, and return */
-    this.$chunker.flush();
-    this.$hash(new DataView(output.buffer, output.byteOffset, output.byteLength));
-    return output;
-
-  };
 
   Hash.prototype.$reset   = function() { throw new Error("Hash $reset not implemented") }
   Hash.prototype.$compute = function() { throw new Error("Hash $compute not implemented") }
