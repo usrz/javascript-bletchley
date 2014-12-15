f0a6d1c755216cc39f138fc8948805b533644efc
Pier Fumagalli <ianosh@gmail.com>
2014-12-15 11:30:14 +0900

    Bind Crypto class.

--- src/crypto/Crypto.js
+++ src/crypto/Crypto.js
@@ -2,13 +2,14 @@
 
 Esquire.define('bletchley/crypto/Crypto', [ 'bletchley/utils/arrays',
                                             'bletchley/codecs',
+                                            'bletchley/utils/BoundClass',
                                             'bletchley/random/Random',
                                             'bletchley/random/SecureRandom',
                                             'bletchley/ciphers/Ciphers',
                                             'bletchley/hashes/Hashes',
                                             'bletchley/hmacs/HMACs',
                                             'bletchley/kdfs/KDFs' ],
-  function(arrays, codecs, Random, SecureRandom, Ciphers, Hashes, HMACs, KDFs) {
+  function(arrays, codecs, BoundClass, Random, SecureRandom, Ciphers, Hashes, HMACs, KDFs) {
 
     return function Crypto(random) {
       if (! random) {
@@ -23,16 +24,18 @@ Esquire.define('bletchley/crypto/Crypto', [ 'bletchley/utils/arrays',
       var kdfs = new KDFs();
 
       Object.defineProperties(this, {
-        "random":    { enumerable: true, configurable: false, value: random.nextBytes  },
-        "stringify": { enumerable: true, configurable: false, value: arrays.decodeUTF8 },
-        "encode":    { enumerable: true, configurable: false, value: codecs.encode     },
-        "decode":    { enumerable: true, configurable: false, value: codecs.decode     },
-        "encrypt":   { enumerable: true, configurable: false, value: ciphers.encrypt   },
-        "decrypt":   { enumerable: true, configurable: false, value: ciphers.decrypt   },
-        "hash":      { enumerable: true, configurable: false, value: hashes.hash       },
-        "hmac":      { enumerable: true, configurable: false, value: hmacs.hmac        },
-        "kdf":       { enumerable: true, configurable: false, value: kdfs.kdf          }
+        "random":    { enumerable: true, configurable: true, value: random.nextBytes  },
+        "stringify": { enumerable: true, configurable: true, value: arrays.decodeUTF8 },
+        "encode":    { enumerable: true, configurable: true, value: codecs.encode     },
+        "decode":    { enumerable: true, configurable: true, value: codecs.decode     },
+        "encrypt":   { enumerable: true, configurable: true, value: ciphers.encrypt   },
+        "decrypt":   { enumerable: true, configurable: true, value: ciphers.decrypt   },
+        "hash":      { enumerable: true, configurable: true, value: hashes.hash       },
+        "hmac":      { enumerable: true, configurable: true, value: hmacs.hmac        },
+        "kdf":       { enumerable: true, configurable: true, value: kdfs.kdf          }
       });
+
+      BoundClass.call(this);
     };
 
   }
